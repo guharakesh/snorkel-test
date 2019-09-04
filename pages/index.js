497bde3b4f6ef3cfee3026b1129eb9a6ad74f0e9
Rakesh Guha <guha.rakesh@gmail.com>
2019-09-04 14:47:04 -0400

    Initialize next

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Filter</title>
+    </Head>
+    <style jsx>{`
+    `}</style>
+  </div>
+)
+
+export default Home
